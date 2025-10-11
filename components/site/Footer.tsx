1d53b6968a8803da4a8efe81a8d28a77512b3ece
Tenpi <pie4chan@gmail.com>
2025-10-11 08:26:44 -0400

    move terms to footer

--- components/site/Footer.tsx
+++ components/site/Footer.tsx
@@ -1,5 +1,7 @@
-import React, { useEffect } from "react"
-import {useThemeSelector, useLayoutSelector, useSearchActions, useThemeActions} from "../../store"
+import React, {useEffect} from "react"
+import {useNavigate} from "react-router-dom"
+import {useThemeSelector, useLayoutSelector, useSearchActions, useThemeActions,
+useLayoutActions} from "../../store"
 import backToTop from "../../assets/icons/backtotop.png"
 import enIcon from "../../assets/icons/translation-en.png"
 import jaIcon from "../../assets/icons/translation-ja.png"
@@ -14,6 +16,8 @@ const Footer: React.FunctionComponent<Props> = (props) => {
     const {setLanguage} = useThemeActions()
     const {mobile} = useLayoutSelector()
     const {setSearch, setSearchFlag, setImageType, setRatingType, setStyleType, setSortType} = useSearchActions()
+    const {setHideMobileNavbar} = useLayoutActions()
+    const navigate = useNavigate()
 
     const getFilter = () => {
         return `hue-rotate(${siteHue - 180}deg) saturate(${siteSaturation}%) brightness(${siteLightness + 70}%)`
@@ -55,32 +59,38 @@ const Footer: React.FunctionComponent<Props> = (props) => {
         <>
         {!props.noPadding ? <div style={{height: "100%", pointerEvents: "none"}}></div> : null}
         <div className="footer">
-            <div className="footer-title-container" onClick={logoClick}>
-                    <span className="footer-title-a">M</span>
-                    <span className="footer-title-b">o</span>
-                    <span className="footer-title-a">e</span>
-                    <span className="footer-title-b">p</span>
-                    <span className="footer-title-a">i</span>
-                    <span className="footer-title-b">c</span>
-                    <span className="footer-title-a">t</span>
-                    <span className="footer-title-b">u</span>
-                    <span className="footer-title-a">r</span>
-                    <span className="footer-title-b">e</span>
-                    <span className="footer-title-a">s</span>
+            <div className="footer-row">
+                <div className="footer-title-container" onClick={logoClick}>
+                        <span className="footer-title-a">M</span>
+                        <span className="footer-title-b">o</span>
+                        <span className="footer-title-a">e</span>
+                        <span className="footer-title-b">p</span>
+                        <span className="footer-title-a">i</span>
+                        <span className="footer-title-b">c</span>
+                        <span className="footer-title-a">t</span>
+                        <span className="footer-title-b">u</span>
+                        <span className="footer-title-a">r</span>
+                        <span className="footer-title-b">e</span>
+                        <span className="footer-title-a">s</span>
+                </div>
+                {!mobile ? <div className="footer-text-container">
+                    <span className="footer-text">- {i18n.footer.bottom} -</span>
+                </div> : null}
+                <div className="footer-container">
+                    <div className="footer-click-container" onClick={() => changeLanguage()}>
+                        <img className="footer-img" src={getLanguageIcon()} style={{height: "20px", marginRight: "5px", filter: getFilter()}}/>
+                        <span className="footer-text">{getLanguageText()}</span>
+                    </div>
+                    <div className="footer-click-container" onClick={() => goToTop()}>
+                        <img className="footer-img" src={backToTop} style={{filter: getFilter()}}/>
+                        <span className="footer-text">{i18n.footer.top}</span>
+                    </div>
+                </div>
             </div>
-            {!mobile ? <div className="footer-text-container">
-                <span className="footer-text">- {i18n.footer.bottom} -</span>
+            {mobile ? <div className="footer-row" style={{justifyContent: "flex-start", marginTop: "3px"}}>
+                <span style={{marginLeft: "12px"}} className="footer-text" onClick={() => {navigate("/terms"); setHideMobileNavbar(true)}}>{i18n.navbar.terms}</span>
+                <span style={{marginLeft: "12px"}} className="footer-text" onClick={() => {navigate("/contact"); setHideMobileNavbar(true)}}>{i18n.navbar.contact}</span>
             </div> : null}
-            <div className="footer-container">
-                <div className="footer-click-container" onClick={() => changeLanguage()}>
-                    <img className="footer-img" src={getLanguageIcon()} style={{height: "20px", marginRight: "5px", filter: getFilter()}}/>
-                    <span className="footer-text">{getLanguageText()}</span>
-                </div>
-                <div className="footer-click-container" onClick={() => goToTop()}>
-                    <img className="footer-img" src={backToTop} style={{filter: getFilter()}}/>
-                    <span className="footer-text">{i18n.footer.top}</span>
-                </div>
-            </div>
         </div>
         </>
     )
