cf24dd29f788cdca6d12664a00b0b67b9b0bacd0
Moebytes <pie4chan@gmail.com>
2025-10-29 16:33:31 -0400

    og:site_name

--- server.tsx
+++ server.tsx
@@ -545,8 +545,8 @@ app.get("/*", async (req: Request, res: Response) => {
           if (!permissions.isMod(req.session)) post = undefined
         }
         if (post) {
-          title = `Moepictures | ${post.englishTitle || post.title}`
-          description = post.englishCommentary || post.commentary || `${post.englishTitle || post.title} by ${post.artist}`
+          title = `${post.englishTitle || post.title} by ${post.artist}`
+          description = post.englishCommentary || post.commentary || ""
           const img = post.images[0]
           image = `${functions.config.getDomain()}/social-preview/${post.postID}${path.extname(img.filename)}`
           url = `${functions.config.getDomain()}/post/${post.postID}/${post.slug}`
