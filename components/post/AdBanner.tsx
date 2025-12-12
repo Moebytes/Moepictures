2a38c0001630f6ffc99872452d229c4103b38030
Moebytes <pie4chan@gmail.com>
2025-12-12 14:02:35 -0500

    .

--- components/post/AdBanner.tsx
+++ components/post/AdBanner.tsx
@@ -18,7 +18,7 @@ const AdBanner: React.FunctionComponent<Props> = (props) => {
 
     return (
         <div className="ad-banner" style={{marginBottom: props.negMargin ? "-10px" : "0px"}}>
-            <ins className="adsbygoogle" style={{display: "flex"}}
+            <ins className="adsbygoogle" style={{display: "block"}}
                 data-ad-client="ca-pub-9022780620749989"
                 data-ad-slot="7308241770"
                 data-ad-format="auto"
