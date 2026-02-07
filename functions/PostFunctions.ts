c1a77fde47a0c922a4c6996f9d25cb30c44bbc3b
Moebytes <pie4chan@gmail.com>
2026-02-07 13:55:16 -0500

    fix slug generation code

--- functions/PostFunctions.ts
+++ functions/PostFunctions.ts
@@ -67,9 +67,10 @@ export default class PostFunctions {
 
     public static generateSlug = (name: string) => {
         let slug = String(name).trim().toLowerCase().replace(/\s+/g, "-").replaceAll("/", "").replaceAll("\\", "")
-        if (slug.startsWith("#")) slug = slug.replaceAll("#", "")
-        if (slug.startsWith("?")) slug = slug.replaceAll("?", "")
-        if (slug.startsWith("&")) slug = slug.replaceAll("&", "")
+        slug = slug.replaceAll("#", "")
+        slug = slug.replaceAll("?", "")
+        slug = slug.replaceAll("&", "")
+        if (!slug) slug = "untitled"
         return slug
     }
 
