cc3529c98c52ef3c4345a3508e8e31fcd83aa233
Moebytes <pie4chan@gmail.com>
2026-03-01 01:21:37 -0500

    remove unused

--- components/image/GridImage.tsx
+++ components/image/GridImage.tsx
@@ -73,15 +73,6 @@ const GridImage = forwardRef<GridWrapperRef, GridWrapperProps>((props, parentRef
         if (img) loadImage()
     }, [img, sizeType])
 
-    /** - Upscaling is slow
-    useEffect(() => {
-        const updateUpscaled = async () => {
-            const upscaled = await functions.shader.anime4kUpscale(img)
-            setUpscaled(upscaled)
-        }
-        if (img) updateUpscaled()
-    }, [img])*/
-
     const getImg = () => {
         return upscaled ? upscaled : img
     }
