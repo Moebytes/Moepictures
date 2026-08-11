52e7c97c851870cec3ecf69dd47ed15620667357
Moebytes <pie4chan@gmail.com>
2026-08-11 14:54:08 -0400

    fix: also fix here

--- pages/item/FavgroupPage.tsx
+++ pages/item/FavgroupPage.tsx
@@ -104,9 +104,7 @@ const FavgroupPage: React.FunctionComponent = () => {
     }
 
     useEffect(() => {
-        if (session.username) {
-            favgroupInfo()
-        }
+        favgroupInfo()
     }, [favgroupName, session])
 
     useEffect(() => {
